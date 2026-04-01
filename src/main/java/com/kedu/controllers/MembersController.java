284261b7b85310f6b6a09d9d415fc6806b7d8c1b
황지혜 <jihye10226@naver.com>
2026-04-01 14:14:43 +0900

    Feat 5.1 로그인/로그아웃 최종본, 메인 수정(#WBS 5.1)

--- src/main/java/com/kedu/controllers/MembersController.java
+++ src/main/java/com/kedu/controllers/MembersController.java
@@ -74,11 +74,11 @@ public class MembersController {
 		}
 		
 		int result = dao.login(mem_id,  mem_password);
-		String nickname = dao.nickname(mem_id); 
-		session.setAttribute("nickname", nickname);
 		
 		if(result == 1) {
-			session.setAttribute("loginId", mem_id);
+			String nickname = dao.nickname(mem_id);
+			session.setAttribute("loginId", mem_id); 
+			session.setAttribute("nickname", nickname);
 			return "redirect:/";
 		} else if(result == 0){
 			rttr.addFlashAttribute("msg", "pwFail");
@@ -88,10 +88,6 @@ public class MembersController {
 		
 		return "redirect:/members/loginUi";
 		}
-//			String nickname = dao.nickname(mem_id); 
-//			session.setAttribute("nickname", nickname);
-	
-	
 	
 	// 회원가입 완료 버튼 클릭 시
 	@RequestMapping("/signup")
