89b545ee0894b3c6307a26c519fffbe1478d90b1
황지혜 <jihye10226@naver.com>
2026-04-02 13:59:41 +0900

    Feat 9.1 모임 만들기 수정10(#WBS 9.1)

--- src/main/java/com/kedu/controllers/MeetingController.java
+++ src/main/java/com/kedu/controllers/MeetingController.java
@@ -21,11 +21,17 @@ public class MeetingController {
 	
 	// 모임 신청 폼 출력
 	@RequestMapping("/list")
-	public String list(Model model) throws Exception {
+	public String list(Model model, HttpSession session) throws Exception {
 			
 		List<MeetingDTO> list = dao.selectAll();
 			
 		model.addAttribute("list", list);
+		
+		String loginId = (String)session.getAttribute("loginId"); 
+		
+		if(loginId == null) { // 로그인을 안한 상태면 로그인 화면으로 보내기
+			return "redirect:/login";
+		}
 			
 		return "meeting/meeting";
 	}
