b5be435dbca9980191da076a591d57404f4ef1be
정선경 <wjdtjsrud000@naver.com>
2026-04-03 12:14:28 +0900

    adminQ&A UI 재정립, 출력 구현 중 3차

--- src/main/java/com/kedu/dao/AdminQaDAO.java
+++ src/main/java/com/kedu/dao/AdminQaDAO.java
@@ -18,6 +18,9 @@ public class AdminQaDAO {
 	public List<QaDTO> selectById() {
 		String sql = "select * from qa order by qa_create_date";
 		return jdbc.query(sql, new BeanPropertyRowMapper<QaDTO>(QaDTO.class));
-		
 	}
+	
+//	public int updateReply() {
+//		String sql = "update qa set admin_answer =?, admin_answer_date = sysdate, qa_status = 1 where qa_seq =?";
+//	}
 }
