2087ca771a9abd48e44a22d043ab06ffa09c686e
든든이 <wjdcofla02@naver.com>
2026-04-03 15:18:40 +0900

    Feat: 6.3. QA 게시판 및 답변 열람기능 - QA게시판 게시글쓰기 insert 구현중 V6 (#WBS 6.3.)

--- src/main/java/com/kedu/dao/UserQaDAO.java
+++ src/main/java/com/kedu/dao/UserQaDAO.java
@@ -16,7 +16,7 @@ public class UserQaDAO {
 	private AdminQaDAO adao;
 	
 	public void insert(QaDTO dto) {
-		String sql = "insert into qa values(qa_seq.nextval,?,?,?,?,sysdate,?)";
+		String sql = "insert into qa values(qa_seq.nextval,?,?,?,?,sysdate,?,null,null,null)";
 		jdbc.update(sql,dto.getMem_id(),dto.getQa_title(),dto.getQa_contents(),dto.getQa_category(),dto.getQa_status());
 	}
 }
