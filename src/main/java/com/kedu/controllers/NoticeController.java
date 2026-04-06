551e78d5a2858c27dd9b46073133585b05e6adf4
든든이 <wjdcofla02@naver.com>
2026-04-06 14:42:58 +0900

    Feat:6.4. - 공지사항 구현중 and admincontroller V2(#WBS_6.4.)

--- src/main/java/com/kedu/controllers/NoticeController.java
+++ src/main/java/com/kedu/controllers/NoticeController.java
@@ -1,11 +1,14 @@
 package com.kedu.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kedu.dao.NoticeDAO;
+import com.kedu.dto.NoticeDTO;
 
 @Controller
 @RequestMapping("/notice")
@@ -15,7 +18,18 @@ public class NoticeController {
 	private NoticeDAO ndao;
 	
 	@RequestMapping("/toNotice")
-	public String toNotice() {
+	public String toNotice(int cPage,Model model) {
+		//list 뽑기
+		int start = (cPage-1)*10+1;
+		int end = cPage*10;
+		List<NoticeDTO> list = ndao.getNoticePost(start,end);
+		
+		//게시글 갯수 가져오기
+		int totalCount = ndao.getTotalNotice();
+		
+		model.addAttribute("cPage",cPage);
+		model.addAttribute("totalCount",totalCount);
+		model.addAttribute("list",list);
 		return "/notice/notice";
 	}
 	
